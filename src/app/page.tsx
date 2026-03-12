44e37f762a297ae1130695a15d83601f8fe08aaf
ford2221 <fordstelle@gmail.com>
2026-03-12 10:28:30 -0400

    translating porfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,10 +14,10 @@ export default function Home() {
   const [loading, setLoading] = useState(false);
 
   const handleValidateField = () => {
-        if (name === "") { return "Nombre obligatorio!" }
-        if (email === "") { return "Email obligatorio!" }
-        if (message === "") { return "mensaje obligatorio!" }
-        if (!validateEmail(email)) { return "Formato de correo invalido!" }
+        if (name === "") { return "Name is required!" }
+        if (email === "") { return "Email is required!" }
+        if (message === "") { return "Message is required!" }
+        if (!validateEmail(email)) { return "Invalid email format!" }
         return ""
     }
 
@@ -56,7 +56,7 @@ export default function Home() {
       
       if (!res.ok) {
         // const errorText = await res.text(); // Loguea el error detallado de Telegram
-        Swal.fire("Error al enviar el mensaje.");
+        Swal.fire("Error sending message.");
         return;
       }
 
@@ -65,7 +65,7 @@ export default function Home() {
       setEmail("");
       setMessage("");
 
-      Swal.fire("Mensaje enviado con exito!");
+      Swal.fire("Message sent successfully!");
     } catch (error) {
       console.log(error);
       // Swal.fire("Ocurrió un error intenta más tarde!");
@@ -93,7 +93,7 @@ export default function Home() {
               />
               <div className='w-64 md:w-2/3'>
                 <h1 className="text-xl text-center md:text-left sm:text-2xl font-bold tracking-widest leading-none" style={{ fontFamily: 'Inter, system-ui' }}>Ford-Ndji Joseph</h1>
-                <p className="text-xs opacity-80 text-center md:text-left mt-1 tracking-widest leading-none">Desarrollador web & móvil • Portafolio retro</p>
+                <p className="text-xs opacity-80 text-center md:text-left mt-1 tracking-widest leading-none">Web & Móvil Developer • Portafolio retro</p>
                 <ul className="space-y-1 text-sm ">
                   <li className="flex items-center text-center md:text-left">
                     <span className="mr-3">
@@ -138,7 +138,7 @@ export default function Home() {
           </header>
 
           <section className="mb-8">
-            <h3 className="text-lg sm:text-xl font-bold mb-2">🎓 Formación</h3>
+            <h3 className="text-lg sm:text-xl font-bold mb-2">🎓 Education</h3>
             <p className="text-sm opacity-90 ml-4">UPTP JJ Montilla, Portuguesa</p>
           </section>
 
@@ -157,7 +157,7 @@ export default function Home() {
           </section>
 
           <section className="mb-8">
-            <h3 className="text-lg sm:text-xl font-bold mb-4">☕ Pasatiempos</h3>
+            <h3 className="text-lg sm:text-xl font-bold mb-4">☕ Hobbies</h3>
             <div className="grid grid-cols-2 sm:grid-cols-4 gap-4 ml-4">
               {hobbies.map((hobby, i) => (
                 <div key={i} className="p-4 rounded-lg border border-slate-700/40 bg-slate-900/40 text-center hover:scale-[1.02] transition-transform">
@@ -169,7 +169,7 @@ export default function Home() {
           </section>
 
           <section className="mb-8">
-            <h3 className="text-lg sm:text-xl font-bold mb-4">🌎 Idiomas</h3>
+            <h3 className="text-lg sm:text-xl font-bold mb-4">🌎 Languages</h3>
             <div className="space-y-3">
               {languages.map((lang, i) => (
                 <div key={i}>
@@ -186,7 +186,7 @@ export default function Home() {
           </section>
 
           <section id="projects" className="mb-12">
-            <h3 className="text-lg sm:text-xl font-bold mb-4">🖥️ Proyectos</h3>
+            <h3 className="text-lg sm:text-xl font-bold mb-4">🖥️ Projects</h3>
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
               {projects.map((p, i) => (
                 <a target='_blank' key={i} href={p.link}   className="p-4 rounded-lg border border-slate-700/40 bg-gradient-to-br from-slate-900/50 to-slate-800/30 hover:scale-[1.01] transition-transform">
@@ -222,7 +222,7 @@ export default function Home() {
                         <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
                         <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
                     </svg>
-                    <p className='ml-4 text-white'>Procesando, Por favor Espere...</p>
+                    <p className='ml-4 text-white'>Wait...</p>
                 </div>
               )}
             </form>
